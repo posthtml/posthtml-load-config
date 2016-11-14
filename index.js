3fad2e5212004043b9eb6fe2a06ded6ff900e6fb
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2016-11-14 13:55:51 +0100

    feat(index): add function support, jsdoc

--- index.js
+++ index.js
@@ -1,21 +1,74 @@
 // ------------------------------------
-// #POSTHTML - LOAD CONFIG
+// #POSTHTML - LOAD CONFIG - INDEX
 // ------------------------------------
 
 'use strict'
 
+const assign = Object.assign
+
 const config = require('cosmiconfig')
 
-const loadOptions = require('posthtml-load-options/lib/loadOptions')
-const loadPlugins = require('./lib/loadPlugins')
+const loadPlugins = require('./lib/plugins.js')
+const loadOptions = require('posthtml-load-options/lib/options.js')
 
-module.exports = function loadConfig (options) {
-  return config('posthtml')
-    .catch((error) => console.log(error))
+/**
+ * @author Michael Ciniawsky (@michael-ciniawsky) <michael.ciniawsky@gmail.com>
+ * @description Autoload Config for PostHTML
+ *
+ * @module posthtml-load-config
+ * @version 1.0.0
+ *
+ * @requires comsiconfig
+ * @requires posthtml-load-options
+ * @requires posthtml-load-plugins
+ *
+ * @method postcssrc
+ *
+ * @param  {Object} ctx Context
+ * @param  {String} path Config Directory
+ * @param  {Object} options Config Options
+ *
+ * @return {Promise} config  PostHTML Plugins, PostHTML Options
+ */
+module.exports = function posthtmlrc (ctx, path, options) {
+  const defaults = { cwd: process.cwd(), env: process.env.NODE_ENV }
+
+  ctx = assign(defaults, ctx)
+  path = path || process.cwd()
+  options = assign({}, options)
+
+  if (ctx.env === undefined) {
+    process.env.NODE_ENV = 'development'
+  }
+
+  return config('posthtml', options)
+    .load(path)
     .then((result) => {
-      return {
-        plugins: loadPlugins(result.config, options),
-        options: loadOptions(result.config, options)
+      if (!result) {
+        console.log(
+          'PostHTML Config could not be loaded. Please check your PostHTML Config.'
+        )
       }
+
+      return result ? result.config : {}
+    })
+    .then((config) => {
+      if (typeof config === 'function') {
+        config = config(ctx)
+      } else {
+        config = assign(config, ctx)
+      }
+
+      if (!config.plugins) {
+        config.plugins = []
+      }
+
+      return {
+        plugins: loadPlugins(config),
+        options: loadOptions(config)
+      }
+    })
+    .catch((err) => {
+      console.log(err)
     })
 }
